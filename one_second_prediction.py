15419fd24dc7d57ae95c9c1e255be962d9727309
Marian Salvan <marian.salvan@gmail.com>
2020-04-25 20:18:03 +0300

    more modifications

--- one_second_prediction.py
+++ one_second_prediction.py
@@ -1,6 +1,6 @@
-import pandas as pd                 # pandas is a dataframe library
+import pandas as pd  # pandas is a dataframe library
 import numpy as np
-import matplotlib.pyplot as plt      # matplotlib.pyplot plots data
+import matplotlib.pyplot as plt  # matplotlib.pyplot plots data
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
@@ -12,105 +12,106 @@ import os
 
 os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 
+OUTPUTS = ['inlet_probe_1_rack_1', 'inlet_probe_2_rack_1', 'inlet_probe_3_rack_1', 'inlet_probe_4_rack_1',
+           'inlet_probe_1_rack_2', 'inlet_probe_2_rack_2', 'inlet_probe_3_rack_2', 'inlet_probe_4_rack_2',
+           'outlet_probe_1_rack_1', 'outlet_probe_1_rack_2', 'outlet_probe_1_room',
+           'outlet_probe_2_room', 'outlet_probe_3_room', 'outlet_probe_4_room']
+
+
 def r2_keras(y_true, y_pred):
     SS_res = K.sum(K.square(y_true - y_pred))
     SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
-    return (1 - SS_res/(SS_tot + K.epsilon()))
+    return (1 - SS_res / (SS_tot + K.epsilon()))
 
 
 def plot_predictions(real, predicted, column):
-    plt.plot(np.arange(120), real, np.arange(120), predicted)
+    plt.plot(np.arange(60), real, np.arange(60), predicted)
     plt.legend(('Real temperature', 'Predicted temperature'))
     plt.xlabel('Sample number')
     plt.ylabel('Temperature')
     plt.title(column)
-    plt.savefig('./images/one_second_inlet_probe_1_rack_1.png')
-    plt.show()
+    plt.savefig('./images/one_second_data/' + column + '.png')
+    # plt.show()
 
 
-df = pd.read_csv("./data/data.log")
+for column in OUTPUTS:
+    df = pd.read_csv("./data/data.log")
 
-feature_col_names = ['t_room_initial', 't_air_input', 'air_flow', 'heat_generation_rate_server_1',
-                     'heat_generation_rate_server_2', 'elapsed_time']
-predicted_class_names = ['inlet_probe_1_rack_1']
+    feature_col_names = ['t_room_initial', 't_air_input', 'air_flow', 'heat_generation_rate_server_1',
+                         'heat_generation_rate_server_2', 'elapsed_time']
 
-X = df[feature_col_names].values      # predictor feature columns (10 X m)
-y = df[predicted_class_names].values  # predicted class column (1 X m)
-split_test_size = 0.30
+    X = df[feature_col_names].values  # predictor feature columns (10 X m)
+    y = df[[column]].values  # predicted class column (1 X m)
+    split_test_size = 0.30
 
-# normalize data
-scaler = MinMaxScaler()
-x_scaled = scaler.fit_transform(X)
-y_scaled = scaler.fit_transform(y)
+    # normalize data
+    scaler = MinMaxScaler()
+    x_scaled = scaler.fit_transform(X)
+    y_scaled = scaler.fit_transform(y)
 
-X_train, X_test, y_train, y_test = train_test_split(x_scaled, y_scaled, test_size=split_test_size, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(x_scaled, y_scaled, test_size=split_test_size, random_state=42)
 
-# X_train = train_df[feature_col_names].values
-# y_train = train_df[predicted_class_names].values
-#
-# X_test = test_df[feature_col_names].values
-# y_test = test_df[predicted_class_names].values
-#
-# scaler = MinMaxScaler()
-# X_train = scaler.fit_transform(X_train)
-# y_train = scaler.fit_transform(y_train)
-#
-# X_test = scaler.fit_transform(X_test)
-# y_test = scaler.fit_transform(y_test)
+    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
-X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+    model = Sequential()
 
-model = Sequential()
+    # input shape = (6,)
+    model.add(LSTM(6, input_shape=(X_train.shape[1], 1), return_sequences=True))
+    model.add(Dropout(0.2))
 
-# input shape = (6,)
-model.add(LSTM(6, input_shape=(X_train.shape[1], 1), return_sequences=True))
-model.add(Dropout(0.2))
+    model.add(LSTM(20, return_sequences=True))
+    model.add(Dropout(0.2))
 
-model.add(LSTM(20, return_sequences=True))
-model.add(Dropout(0.2))
+    model.add(LSTM(20))
+    model.add(Dropout(0.2))
 
-model.add(LSTM(20))
-model.add(Dropout(0.2))
+    model.add(Dense(1, activation='linear'))
 
-model.add(Dense(1, activation='linear'))
+    # compile
+    model.compile(loss='mean_absolute_error',
+                  optimizer='adam',
+                  metrics=[r2_keras])
 
-# compile
-model.compile(loss='mean_absolute_error',
-              optimizer='adam',
-              metrics=[r2_keras])
+    # save model to png
+    plot_model(model, to_file="./images/one_second_prediction.png", show_shapes=True, show_layer_names=True)
 
-# save model to png
-plot_model(model, to_file="./images/one_second_prediction.png", show_shapes=True, show_layer_names=True)
+    # train
+    BATCH_SIZE = 500
+    EPOCHS = 5
 
-# train
-BATCH_SIZE = 500
-EPOCHS = 5
+    cbk_early_stopping = EarlyStopping(monitor='val_r2_keras', mode='max')
 
-cbk_early_stopping = EarlyStopping(monitor='val_r2_keras', mode='max')
+    hist = model.fit(X_train, y_train, BATCH_SIZE, epochs=EPOCHS,
+                     validation_data=(X_test, y_test),
+                     callbacks=[cbk_early_stopping])
 
-hist = model.fit(X_train, y_train, BATCH_SIZE, epochs=EPOCHS,
-          validation_data=(X_test, y_test),
-          callbacks=[cbk_early_stopping])
+    # evaluate the model with the test data to get the scores on "real" data
+    score = model.evaluate(X_test, y_test, verbose=2)
+    print("All score", score)
 
-# evaluate the model with the test data to get the scores on "real" data
-score = model.evaluate(X_test, y_test, verbose=2)
-print("All score", score)
+    print('Test loss:', score[0])
+    print('Test r2: ', score[1])
 
-print('Test loss:', score[0])
-print('Test r2: ', score[1])
+    file = open("./results/one_second_data/result.txt", "a")
+    file.write("Column: " + column + "\n")
+    file.write("Test loss, Test r2" + "\n")
+    file.write(str(score[0]) + " " + str(score[1]) + "\n")
+    file.write("\n")
+    file.close()
 
-# do predictions
-print("Prediction")
-predicted_temperature = model.predict(X_test)
-predicted_temperature = scaler.inverse_transform(predicted_temperature)
-real_temperature = scaler.inverse_transform(y_test)
+    # do predictions
+    print("Prediction")
+    predicted_temperature = model.predict(X_test)
+    predicted_temperature = scaler.inverse_transform(predicted_temperature)
+    real_temperature = scaler.inverse_transform(y_test)
 
-# plot prediction
-plot_predictions(real_temperature[:120], predicted_temperature[:120], 'inlet_probe_1_rack_1')
+    # plot prediction
+    plot_predictions(real_temperature[:60], predicted_temperature[:60], column)
 
-# plot data to see relationships in training and validation data
-epoch_list = list(range(1, len(hist.history['r2_keras']) + 1))  # values for x axis [1, 2, .., # of epochs]
-plt.plot(epoch_list, hist.history['r2_keras'], epoch_list, hist.history['val_r2_keras'])
-plt.legend(('Training r2_keras', 'Validation r2_keras'))
-plt.show()
+    # plot data to see relationships in training and validation data
+    epoch_list = list(range(1, len(hist.history['r2_keras']) + 1))  # values for x axis [1, 2, .., # of epochs]
+    plt.plot(epoch_list, hist.history['r2_keras'], epoch_list, hist.history['val_r2_keras'])
+    plt.legend(('Training r2_keras', 'Validation r2_keras'))
+    plt.savefig('./images/one_second_data/' + column + '_learning.png')
+    # plt.show()
--- preprocessing.py
+++ preprocessing.py
@@ -2,8 +2,14 @@ import pandas as pd  # pandas is a dataframe library
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
-PAST_DATA_SAMPLE_NUMBER = 20
-FUTURE_DATA_SAMPLE_NUMBER = 9
+PAST_DATA_SAMPLE_NUMBER = 40
+FUTURE_DATA_SAMPLE_NUMBER = 19
+
+INLET_TEMPERATURES = ['inlet_probe_1_rack_1', 'inlet_probe_2_rack_1', 'inlet_probe_3_rack_1', 'inlet_probe_4_rack_1',
+                      'inlet_probe_1_rack_2', 'inlet_probe_2_rack_2', 'inlet_probe_3_rack_2', 'inlet_probe_4_rack_2']
+OUTLET_TEMPERATURES = ['outlet_probe_1_rack_1', 'outlet_probe_1_rack_2', 'outlet_probe_1_room',
+                       'outlet_probe_2_room', 'outlet_probe_3_room', 'outlet_probe_4_room']
+
 
 # generate new columns names
 def generate_columns_names(nr, type='inlet_server_past'):
@@ -35,37 +41,83 @@ def create_future_data_columns(df, nr, df_column_name, new_columns_names):
 
     return result
 
-# display all columns
-pd.options.display.max_columns = None
-pd.options.display.max_rows = None
 
-df = pd.read_csv("./data/data.log")
+def create_one_second_dataset(column_list, type='inlet_server', folder="one_second_data"):
+    past_data_sample_number = 4
 
-col_names = ['t_room_initial', 't_air_input', 'air_flow', 'heat_generation_rate_server_1',
-             'heat_generation_rate_server_2', 'inlet_probe_1_rack_1']
+    for column in column_list:
+        # display all columns
+        pd.options.display.max_columns = None
+        pd.options.display.max_rows = None
 
-# drop the other columns
-df.drop(df.columns.difference(col_names), 1, inplace=True)
+        df = pd.read_csv("./data/data.log")
 
-# create the past feature data columns
-past_columns_names = generate_columns_names(PAST_DATA_SAMPLE_NUMBER)
-past_columns = create_past_data_columns(df, PAST_DATA_SAMPLE_NUMBER, col_names[5], past_columns_names)
+        col_names = ['t_room_initial', 't_air_input', 'air_flow', 'heat_generation_rate_server_1',
+                     'heat_generation_rate_server_2', column]
 
-# create the future output data columns
-future_columns_names = generate_columns_names(FUTURE_DATA_SAMPLE_NUMBER, type='inlet_server_future')
-future_columns = create_future_data_columns(df, FUTURE_DATA_SAMPLE_NUMBER, col_names[5], future_columns_names)
+        # drop the other columns
+        df.drop(df.columns.difference(col_names), 1, inplace=True)
 
-# create the new df with past and future data
-past_df = pd.DataFrame(data=past_columns)
-future_df = pd.DataFrame(data=future_columns)
+        # create the past feature data columns
+        past_columns_names = generate_columns_names(past_data_sample_number, type=type + '_past')
+        past_columns = create_past_data_columns(df, past_data_sample_number, col_names[5], past_columns_names)
 
-dataset = pd.concat([past_df, df, future_df], axis=1)
+        # create the new df with past
+        past_df = pd.DataFrame(data=past_columns)
 
-# remove the rows with shifted data - first and last
-dataset.drop(df.head(PAST_DATA_SAMPLE_NUMBER).index, inplace=True)  # drop first n rows
-dataset.drop(df.tail(FUTURE_DATA_SAMPLE_NUMBER).index, inplace=True)  # drop last n rows
+        dataset = pd.concat([past_df, df], axis=1)
 
-print(dataset.shape)
+        # remove the rows with shifted data - first and last
+        dataset.drop(df.head(past_data_sample_number).index, inplace=True)  # drop first n rows
 
-dataset.to_csv("./data/ten_seconds_data/inlet_probe_1_rack_1.csv", index=False)
-print("done")
+        print(dataset.shape)
+
+        dataset.to_csv("./data/" + folder + "/" + column + ".csv", index=False)
+        print("done: " + column)
+
+
+def create_dataset(column_list, type='inlet_server', folder="ten_seconds_data"):
+    for column in column_list:
+        # display all columns
+        pd.options.display.max_columns = None
+        pd.options.display.max_rows = None
+
+        df = pd.read_csv("./data/data.log")
+
+        col_names = ['t_room_initial', 't_air_input', 'air_flow', 'heat_generation_rate_server_1',
+                     'heat_generation_rate_server_2', column]
+
+        # drop the other columns
+        df.drop(df.columns.difference(col_names), 1, inplace=True)
+
+        # create the past feature data columns
+        past_columns_names = generate_columns_names(PAST_DATA_SAMPLE_NUMBER, type=type + '_past')
+        past_columns = create_past_data_columns(df, PAST_DATA_SAMPLE_NUMBER, col_names[5], past_columns_names)
+
+        # create the future output data columns
+        future_columns_names = generate_columns_names(FUTURE_DATA_SAMPLE_NUMBER, type=type + '_future')
+        future_columns = create_future_data_columns(df, FUTURE_DATA_SAMPLE_NUMBER, col_names[5], future_columns_names)
+
+        # create the new df with past and future data
+        past_df = pd.DataFrame(data=past_columns)
+        future_df = pd.DataFrame(data=future_columns)
+
+        dataset = pd.concat([past_df, df, future_df], axis=1)
+
+        # remove the rows with shifted data - first and last
+        dataset.drop(df.head(PAST_DATA_SAMPLE_NUMBER).index, inplace=True)  # drop first n rows
+        dataset.drop(df.tail(FUTURE_DATA_SAMPLE_NUMBER).index, inplace=True)  # drop last n rows
+
+        print(dataset.shape)
+
+        dataset.to_csv("./data/" + folder + "/" + column + ".csv", index=False)
+        print("done: " + column)
+
+
+create_dataset(INLET_TEMPERATURES, type="inlet_server", folder="twenty_seconds_data")
+# create_one_second_dataset(INLET_TEMPERATURES, type="inlet_server")
+print("Done inlet")
+
+create_dataset(OUTLET_TEMPERATURES, type="outlet_server", folder="twenty_seconds_data")
+# create_one_second_dataset(OUTLET_TEMPERATURES, type="outlet_server")
+print("Done outlet")

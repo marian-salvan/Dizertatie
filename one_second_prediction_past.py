00ac21c19a1231872b0b08ed9c001cd43ea0bd9a
Marian Salvan <marian.salvan@gmail.com>
2020-04-26 19:12:54 +0300

    more results + started writing on Documentation

--- one_second_prediction_past.py
+++ one_second_prediction_past.py
@@ -12,118 +12,112 @@ import os
 
 os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 
+OUTPUTS = ['inlet_probe_1_rack_1', 'inlet_probe_2_rack_1', 'inlet_probe_3_rack_1', 'inlet_probe_4_rack_1',
+           'inlet_probe_1_rack_2', 'inlet_probe_2_rack_2', 'inlet_probe_3_rack_2', 'inlet_probe_4_rack_2',
+           'outlet_probe_1_rack_1', 'outlet_probe_1_rack_2', 'outlet_probe_1_room',
+           'outlet_probe_2_room', 'outlet_probe_3_room', 'outlet_probe_4_room']
+
+
 def r2_keras(y_true, y_pred):
     SS_res =  K.sum(K.square(y_true - y_pred))
     SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
     return ( 1 - SS_res/(SS_tot + K.epsilon()) )
 
+
 def plot_predictions(real, predicted, column):
-    plt.plot(np.arange(120), real, np.arange(120), predicted)
-    plt.legend(('Real temperature', 'Predicted temperature'))
-    plt.xlabel('Sample number')
-    plt.ylabel('Temperature')
-    plt.title(column)
-    plt.savefig('./images/one_second_past_inlet_probe_1_rack_1.png')
-    plt.show()
-
-    # Create just a figure and only one subplot
     fig, ax = plt.subplots()
-    ax.plot(x, y)
-    ax.set_title('Simple plot')
-    ax.set_xlabel('test')
-    ax.set_ylabel('test')
-
-    plt.show()
+    line1, = ax.plot(np.arange(60), real)
+    line2, = ax.plot(np.arange(60), predicted)
+    line1.set_label('Real temperature')
+    line2.set_label('Predicted temperature')
+    ax.set_title(column)
+    ax.set_xlabel('Sample number')
+    ax.set_ylabel('Temperature')
+    ax.legend()
+    plt.savefig('./images/one_second_data_past/' + column + '.png')
     plt.close(fig)
 
 
-df = pd.read_csv("./data/data_1_second.csv")
+for column in OUTPUTS:
 
-# display all columns
-# pd.options.display.max_columns = None
-# pd.options.display.max_rows = None
+    df = pd.read_csv("E:/data/one_second_data/" + column + ".csv")
 
-print(df.shape)
-print(df.head(5))
-# print(df.corr())
-print(df.isnull().values.any())
+    X = df[df.columns[0:9]].values      # predictor feature columns (10 X m)
+    y = df[df.columns[9:10]].values  # predicted class column (1 X m)
+    split_test_size = 0.30
 
-# split data into test and validation using sklearn
-feature_col_names = ['inlet_server_past_1', 'inlet_server_past_2',
-                     'inlet_server_past_3', 'inlet_server_past_4',
-                     't_room_initial', 't_air_input', 'air_flow',
-                     'heat_generation_rate_server_1', 'heat_generation_rate_server_2']
-predicted_class_names = ['inlet_probe_1_rack_1']
+    # normalize data
+    scaler = MinMaxScaler()
+    x_scaled = scaler.fit_transform(X)
+    y_scaled = scaler.fit_transform(y)
 
-X = df[feature_col_names].values      # predictor feature columns (10 X m)
-y = df[predicted_class_names].values  # predicted class column (1 X m)
-split_test_size = 0.30
+    X_train, X_test, y_train, y_test = train_test_split(x_scaled, y_scaled, test_size=split_test_size, random_state=42)
 
-# normalize data
-scaler = MinMaxScaler()
-x_scaled = scaler.fit_transform(X)
-y_scaled = scaler.fit_transform(y)
+    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
-X_train, X_test, y_train, y_test = train_test_split(x_scaled, y_scaled, test_size=split_test_size, random_state=42)
+    print(X_train.shape)
+    print(X_test.shape)
 
-X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+    model = Sequential()
 
-print(X_train.shape)
-print(X_test.shape)
+    # input shape = (6,)
+    model.add(LSTM(9, input_shape=(X_train.shape[1], 1), return_sequences=True))
+    model.add(Dropout(0.2))
 
-model = Sequential()
+    model.add(LSTM(18, return_sequences=True))
+    model.add(Dropout(0.2))
 
-# input shape = (6,)
-model.add(LSTM(9, input_shape=(X_train.shape[1], 1), return_sequences=True))
-model.add(Dropout(0.2))
+    model.add(LSTM(18))
+    model.add(Dropout(0.2))
 
-model.add(LSTM(18, return_sequences=True))
-model.add(Dropout(0.2))
+    model.add(Dense(1, activation='linear'))
 
-model.add(LSTM(18))
-model.add(Dropout(0.2))
+    # compile
+    model.compile(loss='mean_absolute_error',
+                  optimizer='adam',
+                  metrics=[r2_keras])
 
-model.add(Dense(1, activation='linear'))
+    # save model to png
+    plot_model(model, to_file="./images/one_second_prediction_past.png", show_shapes=True, show_layer_names=True)
 
-# compile
-model.compile(loss='mean_absolute_error',
-              optimizer='adam',
-              metrics=[r2_keras])
+    # train
+    BATCH_SIZE = 500
+    EPOCHS = 5
 
-# save model to png
-plot_model(model, to_file="./images/one_second_prediction_past.png", show_shapes=True, show_layer_names=True)
+    cbk_early_stopping = EarlyStopping(monitor='val_r2_keras', mode='min')
 
-# train
-BATCH_SIZE = 500
-EPOCHS = 5
+    hist = model.fit(X_train, y_train, BATCH_SIZE, epochs=EPOCHS,
+              validation_data=(X_test, y_test),
+              callbacks=[cbk_early_stopping])
 
-cbk_early_stopping = EarlyStopping(monitor='val_r2_keras', mode='min')
+    # save transfer learning model
+    model.save('first-try.model')
+    # evaluate the model with the test data to get the scores on "real" data
+    score = model.evaluate(X_test, y_test, verbose=0)
+    print("All score", score)
 
-hist = model.fit(X_train, y_train, BATCH_SIZE, epochs=EPOCHS,
-          validation_data=(X_test, y_test),
-          callbacks=[cbk_early_stopping])
+    print('Test loss:', score[0])
+    print('Test mae: ', score[1])
 
-# save transfer learning model
-model.save('first-try.model')
-# evaluate the model with the test data to get the scores on "real" data
-score = model.evaluate(X_test, y_test, verbose=0)
-print("All score", score)
+    file = open("./results/one_second_data_past/result.txt", "a")
+    file.write("Column: " + column + "\n")
+    file.write("Test loss, Test r2" + "\n")
+    file.write(str(score[0]) + " " + str(score[1]) + "\n")
+    file.write("\n")
+    file.close()
 
-print('Test loss:', score[0])
-print('Test mae: ', score[1])
+    # do predictions
+    print("Prediction")
+    predicted_temperature = model.predict(X_test)
+    predicted_temperature = scaler.inverse_transform(predicted_temperature)
+    real_temperature = scaler.inverse_transform(y_test)
 
-# do predictions
-print("Prediction")
-predicted_temperature = model.predict(X_test)
-predicted_temperature = scaler.inverse_transform(predicted_temperature)
-real_temperature = scaler.inverse_transform(y_test)
+    # plot prediction
+    plot_predictions(real_temperature[:60], predicted_temperature[:60], column)
 
-# plot prediction
-plot_predictions(real_temperature[:120], predicted_temperature[:120], 'inlet_probe_1_rack_1')
-
-# plot data to see relationships in training and validation data
-epoch_list = list(range(1, len(hist.history['r2_keras']) + 1))  # values for x axis [1, 2, .., # of epochs]
-plt.plot(epoch_list, hist.history['r2_keras'], epoch_list, hist.history['val_r2_keras'])
-plt.legend(('Training r2_keras', 'Validation r2_keras'))
-plt.show()
+    # plot data to see relationships in training and validation data
+    # epoch_list = list(range(1, len(hist.history['r2_keras']) + 1))  # values for x axis [1, 2, .., # of epochs]
+    # plt.plot(epoch_list, hist.history['r2_keras'], epoch_list, hist.history['val_r2_keras'])
+    # plt.legend(('Training r2_keras', 'Validation r2_keras'))
+    # plt.show()
--- ten_seconds_prediction.py
+++ ten_seconds_prediction.py
@@ -12,92 +12,114 @@ import os
 
 os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 
+OUTPUTS = ['inlet_probe_1_rack_1', 'inlet_probe_2_rack_1', 'inlet_probe_3_rack_1', 'inlet_probe_4_rack_1',
+           'inlet_probe_1_rack_2', 'inlet_probe_2_rack_2', 'inlet_probe_3_rack_2', 'inlet_probe_4_rack_2',
+           'outlet_probe_1_rack_1', 'outlet_probe_1_rack_2', 'outlet_probe_1_room',
+           'outlet_probe_2_room', 'outlet_probe_3_room', 'outlet_probe_4_room']
+
+
 def r2_keras(y_true, y_pred):
     SS_res = K.sum(K.square(y_true - y_pred))
     SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
     return (1 - SS_res/(SS_tot + K.epsilon()))
 
-
 def plot_predictions(real, predicted, column):
-    plt.plot(np.arange(10), real, np.arange(10), predicted)
-    plt.legend(('Real temperature', 'Predicted temperature'))
-    plt.xlabel('Second')
-    plt.ylabel('Temperature')
-    plt.title(column)
-    plt.savefig('./images/ten_seconds_inlet_probe_1_rack_1.png')
-    plt.show()
+    fig, ax = plt.subplots()
+    line1, = ax.plot(np.arange(10), real)
+    line2, = ax.plot(np.arange(10), predicted)
+    line1.set_label('Real temperature')
+    line2.set_label('Predicted temperature')
+    ax.set_title(column)
+    ax.set_xlabel('Second')
+    ax.set_ylabel('Temperature')
+    ax.legend()
+    plt.savefig('./images/ten_seconds_data/' + column + '.png')
+    plt.close(fig)
 
 
-df = pd.read_csv("./data/ten_seconds_data/inlet_probe_1_rack_1.csv")
-print(df.shape)
+prediction_index = 0
 
-X = df[df.columns[0:25]].values      # predictor feature columns (10 X m)
-y = df[df.columns[25:35]].values  # predicted class column (1 X m)
-split_test_size = 0.30
+for column in OUTPUTS:
 
-# normalize data
-scaler = MinMaxScaler()
-x_scaled = scaler.fit_transform(X)
-y_scaled = scaler.fit_transform(y)
+    df = pd.read_csv("E:/data/ten_seconds_data/" + column + ".csv")
+    print(df.shape)
 
-X_train, X_test, y_train, y_test = train_test_split(x_scaled, y_scaled, test_size=split_test_size, random_state=42)
+    X = df[df.columns[0:25]].values      # predictor feature columns (10 X m)
+    y = df[df.columns[25:35]].values  # predicted class column (1 X m)
+    split_test_size = 0.30
 
-X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+    # normalize data
+    scaler = MinMaxScaler()
+    x_scaled = scaler.fit_transform(X)
+    y_scaled = scaler.fit_transform(y)
 
-model = Sequential()
+    X_train, X_test, y_train, y_test = train_test_split(x_scaled, y_scaled, test_size=split_test_size, random_state=42)
 
-# input shape = (6,)
-model.add(LSTM(25, input_shape=(X_train.shape[1], 1), return_sequences=True))
-model.add(Dropout(0.2))
+    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
-model.add(LSTM(50, return_sequences=True))
-model.add(Dropout(0.2))
+    model = Sequential()
 
-model.add(LSTM(50))
-model.add(Dropout(0.2))
+    # input shape = (6,)
+    model.add(LSTM(25, input_shape=(X_train.shape[1], 1), return_sequences=True))
+    model.add(Dropout(0.2))
 
-model.add(Dense(10, activation='linear'))
+    model.add(LSTM(50, return_sequences=True))
+    model.add(Dropout(0.2))
 
-# compile
-model.compile(loss='mean_absolute_error',
-              optimizer='adam',
-              metrics=[r2_keras])
+    model.add(LSTM(50))
+    model.add(Dropout(0.2))
 
-# save model to png
-plot_model(model, to_file="./images/ten_seconds_prediction.png", show_shapes=True, show_layer_names=True)
+    model.add(Dense(10, activation='linear'))
 
-# train
-BATCH_SIZE = 500
-EPOCHS = 5
+    # compile
+    model.compile(loss='mean_absolute_error',
+                  optimizer='adam',
+                  metrics=[r2_keras])
 
-cbk_early_stopping = EarlyStopping(monitor='val_r2_keras', mode='max')
+    # save model to png
+    plot_model(model, to_file="./images/ten_seconds_prediction.png", show_shapes=True, show_layer_names=True)
 
-hist = model.fit(X_train, y_train, BATCH_SIZE, epochs=EPOCHS,
-          validation_data=(X_test, y_test),
-          callbacks=[cbk_early_stopping])
+    # train
+    BATCH_SIZE = 500
+    EPOCHS = 5
 
-# evaluate the model with the test data to get the scores on "real" data
-score = model.evaluate(X_test, y_test, verbose=2)
-print("All score", score)
+    cbk_early_stopping = EarlyStopping(monitor='val_r2_keras', mode='max')
 
-print('Test loss:', score[0])
-print('Test r2: ', score[1])
+    hist = model.fit(X_train, y_train, BATCH_SIZE, epochs=EPOCHS,
+              validation_data=(X_test, y_test),
+              callbacks=[cbk_early_stopping])
 
-# do predictions
-print("Prediction")
-predicted_temperature = model.predict(X_test)
-predicted_temperature = scaler.inverse_transform(predicted_temperature)
-real_temperature = scaler.inverse_transform(y_test)
+    # evaluate the model with the test data to get the scores on "real" data
+    score = model.evaluate(X_test, y_test, verbose=2)
+    print("All score", score)
 
-# plot prediction
-print('Predicted: ', predicted_temperature[0])
-print('Real: ', real_temperature[0])
+    print('Test loss:', score[0])
+    print('Test r2: ', score[1])
 
-plot_predictions(real_temperature[0], predicted_temperature[0], 'inlet_probe_1_rack_1')
+    # do predictions
+    print("Prediction")
+    predicted_temperature = model.predict(X_test)
+    predicted_temperature = scaler.inverse_transform(predicted_temperature)
+    real_temperature = scaler.inverse_transform(y_test)
 
-# plot data to see relationships in training and validation data
-epoch_list = list(range(1, len(hist.history['r2_keras']) + 1))  # values for x axis [1, 2, .., # of epochs]
-plt.plot(epoch_list, hist.history['r2_keras'], epoch_list, hist.history['val_r2_keras'])
-plt.legend(('Training r2_keras', 'Validation r2_keras'))
-plt.show()
+    # plot prediction
+    print('Predicted: ', predicted_temperature[0])
+    print('Real: ', real_temperature[0])
+
+    file = open("./results/ten_seconds_data/result.txt", "a")
+    file.write("Column: " + column + "\n")
+    file.write("Test loss, Test r2" + "\n")
+    file.write(str(score[0]) + " " + str(score[1]) + "\n")
+    file.write("\n")
+    file.close()
+
+    plot_predictions(real_temperature[prediction_index], predicted_temperature[prediction_index], column)
+
+    prediction_index = prediction_index + 1
+
+    # plot data to see relationships in training and validation data
+    # epoch_list = list(range(1, len(hist.history['r2_keras']) + 1))  # values for x axis [1, 2, .., # of epochs]
+    # plt.plot(epoch_list, hist.history['r2_keras'], epoch_list, hist.history['val_r2_keras'])
+    # plt.legend(('Training r2_keras', 'Validation r2_keras'))
+    # plt.show()
